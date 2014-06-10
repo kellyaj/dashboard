61ef82a18f90bd2e6bd37dfefbd5c8ca3ed231ce
Andrew Kelly <akelly@8thlight.com>
2014-06-10 08:48:42 -0500

    includes icon

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ kelvinToFahrenheit = function(kelvin_temp) {
 $.ajax({
   url: "http://api.openweathermap.org/data/2.5/weather?q=chicago,il"
 }).done(function(data) {
+  var weatherInfo = data.weather[0];
+  var description = weatherInfo.description;
   var kelvin_temp = data.main.temp;
   var fahrenheit_temp = kelvinToFahrenheit(kelvin_temp);
-  $('[data-id=temp]').html("Temp: " + fahrenheit_temp)
+  var weatherIconUrl = "http://openweathermap.org/img/w/" + weatherInfo.icon + ".png";
+  $('[data-id=weather-icon]').attr("src", weatherIconUrl);
+  $('[data-id=temp]').html(fahrenheit_temp);
+  $('[data-id=weather-description]').html(description);
 });
 

e6c23c7a7139b1cc1fe841db2b2ba0480403f7eb
Andrew Kelly <akelly@8thlight.com>
2014-06-10 16:19:28 -0500

    uses geolocation instead of hardcoded Chicago

--- main.js
+++ main.js
@@ -2,17 +2,23 @@ kelvinToFahrenheit = function(kelvinTemp) {
   return Math.floor((kelvinTemp - 273) * 1.8 + 32);
 };
 
-$.ajax({
-  url: "http://api.openweathermap.org/data/2.5/weather?q=chicago,il"
-}).done(function(data) {
-  var weatherInfo = data.weather[0];
-  var description = weatherInfo.description;
-  var kelvinTemp = data.main.temp;
-  var fahrenheitTemp = kelvinToFahrenheit(kelvinTemp);
-  var weatherIconUrl = "http://openweathermap.org/img/w/" + weatherInfo.icon + ".png";
-  var degreeHex = '\xB0'
+navigator.geolocation.getCurrentPosition(function(position) {
+  requestWeather(position.coords.latitude, position.coords.longitude);
+});
+
+requestWeather = function(latitude, longitude) {
+  var requestUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude;
+  $.ajax({
+    url: requestUrl
+  }).done(function(data) {
+    var weatherInfo = data.weather[0];
+    var description = weatherInfo.description;
+    var kelvinTemp = data.main.temp;
+    var fahrenheitTemp = kelvinToFahrenheit(kelvinTemp);
+    var weatherIconUrl = "http://openweathermap.org/img/w/" + weatherInfo.icon + ".png";
+    var degreeHex = '\xB0';
   $('[data-id=weather-icon]').attr("src", weatherIconUrl);
   $('[data-id=temp]').html(fahrenheitTemp + degreeHex);
   $('[data-id=weather-description]').html(description.toUpperCase());
-});
-
+  })
+}
